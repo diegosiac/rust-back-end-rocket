23716c7ac7bffc338540173289e8fd49f99d1ab4
diegosiac <diego35502@hotmail.com>
2024-04-21 11:26:00 -0600

    Refactor main.rs to add task creation endpoint in Rocket web framework

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate diesel;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hola, mundo con Rocket!"
+mod schema;
+
+use rocket::serde::json::Json;
+
+use diesel::prelude::*;
+use rocket_sync_db_pools::{database, diesel::PgConnection};
+
+use schema::tasks;
+use serde::{Deserialize, Serialize};
+
+#[database("postgres_database")]
+struct DbConn(PgConnection);
+
+#[derive(Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "tasks"]
+struct Task {
+    id: i32,
+    title: String,
+    description: String,
+    completed: bool,
+}
+
+#[post("/tasks", format = "json", data = "<task>")]
+async fn create_task(task: Json<Task>, db: DbConn) -> Result<Json<Task>, String> {
+    db.run(move |c| {
+        diesel::insert_into(tasks::table)
+            .values(task.into_inner())
+            .get_result(c)
+            .map_err(|_| "Error adding task".to_string())
+            .map(Json)
+    })
+    .await
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .attach(DbConn::fairing())
+        .mount("/", routes![create_task])
 }
