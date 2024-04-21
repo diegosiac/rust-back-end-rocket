30643dc221db277e2200dd01bc070774f0e39d0c
diegosiac <diego35502@hotmail.com>
2024-04-21 11:25:43 -0600

    Add tasks table to database schema

--- migrations/2024-04-21-081236_create_tasks/up.sql
+++ migrations/2024-04-21-081236_create_tasks/up.sql
@@ -1,1 +1,8 @@
 -- Your SQL goes here
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT FALSE
+);
